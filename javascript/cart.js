fc7b7b7d481482f29cd803e0ecd29c46a26be05e
nataliarodcha <a01383192@tec.mx>
2023-12-03 18:06:29 +0100

    detail page

--- javascript/cart.js
+++ javascript/cart.js
@@ -4,6 +4,7 @@ const cartButton = document.querySelector(".cart-button");
 const closeCartButton = document.querySelector(".close-cart-button");
 const optionButton1 = document.querySelector(".product-option-button1");
 const optionButton2 = document.querySelector(".product-option-button2");
+const optionButton3 = document.querySelector(".product-option-button3");
 
 const addToCart = function () {
   cart.classList.remove("hidden");
@@ -16,20 +17,26 @@ const chooseOption = function (option) {
   if (option === 1) {
     optionButton1.classList.add("active-option");
     optionButton2.classList.remove("active-option");
+    optionButton3.classList.remove("active-option");
   } else if (option === 2) {
     optionButton1.classList.remove("active-option");
     optionButton2.classList.add("active-option");
+    optionButton3.classList.remove("active-option");
   } else {
     optionButton1.classList.remove("active-option");
     optionButton2.classList.remove("active-option");
+    optionButton3.classList.add("active-option");
   }
-}
+};
 
-optionButton1.addEventListener("click", function() {
+optionButton1.addEventListener("click", function () {
   chooseOption(1);
 });
-optionButton2.addEventListener("click", function() {
+optionButton2.addEventListener("click", function () {
   chooseOption(2);
+});
+optionButton3.addEventListener("click", function () {
+  chooseOption(3);
 });
 
 const closeCart = function () {
